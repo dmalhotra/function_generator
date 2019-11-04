3d3c2ee23cab0504d566de97c348d308b22ef82e
Robert Blackwell <rblackwell@flatironinstitute.org>
2019-11-04 14:33:44 -0500

    Add general Vandermonde implementation for C++ version

--- src/function_generator.cpp
+++ src/function_generator.cpp
@@ -1,9 +1,10 @@
 #include <algorithm>
+#include <chrono>
 #include <cmath>
 #include <eigen3/Eigen/Eigen>
 #include <eigen3/Eigen/LU>
 #include <iostream>
-#include <iterator>
+#include <random>
 #include <string>
 #include <vector>
 
@@ -11,41 +12,34 @@
 
 typedef Eigen::VectorXd vecxd;
 
-double Vandermonde[8 * 8] = {
-    1.,          1.,          1.,          1.,          1.,
-    1.,          1.,          1.,          -0.98078528, -0.83146961,
-    -0.55557023, -0.19509032, 0.19509032,  0.55557023,  0.83146961,
-    0.98078528,  0.92387953,  0.38268343,  -0.38268343, -0.92387953,
-    -0.92387953, -0.38268343, 0.38268343,  0.92387953,  -0.83146961,
-    0.19509032,  0.98078528,  0.55557023,  -0.55557023, -0.98078528,
-    -0.19509032, 0.83146961,  0.70710678,  -0.70710678, -0.70710678,
-    0.70710678,  0.70710678,  -0.70710678, -0.70710678, 0.70710678,
-    -0.55557023, 0.98078528,  -0.19509032, -0.83146961, 0.83146961,
-    0.19509032,  -0.98078528, 0.55557023,  0.38268343,  -0.92387953,
-    0.92387953,  -0.38268343, -0.38268343, 0.92387953,  -0.92387953,
-    0.38268343,  -0.19509032, 0.55557023,  -0.83146961, 0.98078528,
-    -0.98078528, 0.83146961,  -0.55557023, 0.19509032};
-
 class FunctionGenerator {
   public:
     FunctionGenerator(double (*f)(double), double a, double b,
                       double tol = 1e-10, int n = 8, double mw = 1e-15)
         : f_(f), a_(a), b_(b), tol_(tol), n_(n), mw_(mw) {
 
-        double r = b - a;
-        double a1 = a + r / 3;
-        double a2 = a + 2 * r / 3;
-
-        x_ = get_chebyshev_nodes(a, b, n);
-        V_ = Eigen::Map<Eigen::MatrixXd>(Vandermonde, 8, 8);
+        init_vandermonde();
         VLU_ = Eigen::PartialPivLU<Eigen::MatrixXd>(V_);
 
         fit(a_, b_);
     };
 
-    double operator()(double x) {
-        auto el = std::lower_bound(lbs_.begin(), lbs_.end(), x);
-        size_t index = std::distance(lbs_.begin(), el);
+    int bisect(double x) {
+        size_t n1 = 0;
+        size_t n2 = lbs_.size();
+        while (n2 - n1 > 1) {
+            const size_t m = n1 + (n2 - n1) / 2;
+            if (x < lbs_[m])
+                n2 = m;
+            else
+                n1 = m;
+        }
+
+        return n1;
+    }
+
+    inline double operator()(double x) {
+        int index = bisect(x);
         double a = lbs_[index];
         double b = ubs_[index];
         double xinterp = 2 * (x - a) / (b - a) - 1.0;
@@ -61,7 +55,6 @@ class FunctionGenerator {
     const int n_;
     const double mw_;
 
-    Eigen::VectorXd x_;
     Eigen::MatrixXd V_;
     Eigen::PartialPivLU<Eigen::MatrixXd> VLU_;
 
@@ -83,6 +76,22 @@ class FunctionGenerator {
         return c0 + c1 * x;
     }
 
+    void init_vandermonde() {
+        V_.resize(n_, n_);
+        vecxd x = get_chebyshev_nodes(-1, 1, n_);
+        for (int j = 0; j < n_; ++j) {
+            V_(0, j) = 1;
+            V_(1, j) = x(j);
+        }
+
+        for (int i = 2; i < n_; ++i) {
+            for (int j = 0; j < n_; ++j) {
+                V_(i, j) = V_(i - 1, j) * 2 * x(j) - V_(i - 2, j);
+            }
+        }
+        V_ = V_.transpose().eval();
+    }
+
     vecxd get_chebyshev_nodes(double lb, double ub, int order) {
         vecxd res(order);
         for (int k = 0; k < order; ++k)
@@ -95,7 +104,7 @@ class FunctionGenerator {
 
     void fit(double a, double b) {
         double m = 0.5 * (a + b);
-        std::cout << "[" << a << ", " << b << "]\n";
+        //        std::cout << "[" << a << ", " << b << "]\n";
 
         vecxd fx = get_chebyshev_nodes(a, b, n_);
 
@@ -125,9 +134,53 @@ class FunctionGenerator {
 };
 
 int main(int argc, char *argv[]) {
-    FunctionGenerator myFunc(exp, 0, 1);
+    using namespace std::chrono;
+    high_resolution_clock::time_point start = high_resolution_clock::now();
 
-    std::cout << myFunc(0.5) << std::endl;
+    double range[2] = {1e-10, 1000};
+    FunctionGenerator myFunc(log, range[0], range[1], 1e-10, 12);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(range[0], range[1]);
+    size_t n_el = 100000000;
+    std::vector<double> x(n_el);
+    for (size_t i = 0; i < n_el; ++i)
+        x[i] = dis(gen);
+    high_resolution_clock::time_point finish = high_resolution_clock::now();
+    duration<double> time_span =
+        duration_cast<duration<double>>(finish - start);
+
+    std::cout << "RNG generation took " << time_span.count() << " seconds.\n";
+
+    {
+        high_resolution_clock::time_point start = high_resolution_clock::now();
+
+        for (auto i = 0; i < n_el; ++i) {
+            log(x[i]);
+        }
+        high_resolution_clock::time_point finish = high_resolution_clock::now();
+        duration<double> time_span =
+            duration_cast<duration<double>>(finish - start);
+
+        std::cout << "System 'log' took " << time_span.count() << " seconds.\n";
+    }
+
+    {
+        high_resolution_clock::time_point start = high_resolution_clock::now();
+
+        for (auto i = 0; i < n_el; ++i) {
+            myFunc(x[i]);
+        }
+        high_resolution_clock::time_point finish = high_resolution_clock::now();
+        duration<double> time_span =
+            duration_cast<duration<double>>(finish - start);
+
+        std::cout << "Chebyshev 'log' took " << time_span.count()
+                  << " seconds.\n";
+        std::cout << "Efficiency: " << n_el / time_span.count()
+                  << " evaluations / sec\n";
+    }
 
     return 0;
 }
