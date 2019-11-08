1bc5fd10614005f47aa87017504a930c4a4911ca
Robert Blackwell <rblackwell@flatironinstitute.org>
2019-11-08 15:36:35 -0500

    Add loads of test cases to C++ tester

--- src/function_generator.cpp
+++ src/function_generator.cpp
@@ -12,8 +12,10 @@ PYBIND11_MODULE(FunctionGenerator, m) {
              py::arg("fpy") = "", py::arg("a") = 0.0, py::arg("b") = 1.0,
              py::arg("tol") = 1e-10, py::arg("mw") = 1e-15)
         // .def("__call__", &FunctionGenerator::operator())
-        // .def("__call__", py::vectorize(&FunctionGenerator::operator()))
+        //.def("__call__", py::vectorize(&FunctionGenerator<ORDER, TABLE_SIZE>::operator()))
+        .def("__call__", &FunctionGenerator<ORDER, TABLE_SIZE>::operator())
         .def("__call__", &FunctionGenerator<ORDER, TABLE_SIZE>::arr_call)
+        ;
     ;
 }
 #undef ORDER
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <cmath>
+#include <fstream>
 #include <functional>
 #include <iostream>
 #include <random>
@@ -8,76 +9,114 @@
 #include "function_generator.hpp"
 #include <omp.h>
 
-template class FunctionGenerator<8, 4096>;
+#include <gsl/gsl_sf.h>
+
+typedef std::chrono::high_resolution_clock clk;
+typedef std::chrono::duration<double> duration;
+
+typedef struct {
+    std::string name;
+    double low;
+    double high;
+    std::function<double(double)> f;
+} func_t;
+
+template <int n_, int table_size_>
+void timeit(func_t func, std::vector<double> &xraw) {
+    FunctionGenerator<n_, table_size_> f(func.f, func.low, func.high, 1E-12);
+    const double range = func.high - func.low;
+
+    std::vector<double> x = xraw;
+    for (auto &xel : x)
+        xel = xel * range + func.low;
+
+    std::cout << "First 5 test value deltas\n";
+    for (auto i = 0; i < 5; ++i) {
+        double yExp = f(x[i]);
+        double yLib = func.f(x[i]);
+        std::cout << "\t" << std::fabs((yExp - yLib) / yLib) << std::endl;
+    }
+
+    int n_loops = 10;
+    std::vector<double> res(x.size());
+
+    {
+        clk::time_point start = clk::now();
+
+        for (int irun = 0; irun < n_loops; ++irun) {
+            for (size_t i = 0; i < x.size(); ++i) {
+                res[i] = func.f(x[i]);
+            }
+        }
+
+        clk::time_point finish = clk::now();
+        duration time_span =
+            std::chrono::duration_cast<duration>(finish - start);
+
+        std::cout << "System Efficiency: "
+                  << x.size() * n_loops / time_span.count() / 1e6
+                  << " million evaluations / sec\n";
+
+        std::ofstream f_out(func.name + "_system.dat");
+        for (size_t i = 0; i < 1000; ++i) {
+            double a = func.low + i * range / 1000;
+            f_out << a << "\t" << func.f(a) << std::endl;
+        }
+    }
+
+    {
+        clk::time_point start = clk::now();
+
+        for (int irun = 0; irun < n_loops; ++irun) {
+            for (size_t i = 0; i < x.size(); ++i) {
+                res[i] = f(x[i]);
+            }
+        }
+
+        clk::time_point finish = clk::now();
+        duration time_span =
+            std::chrono::duration_cast<duration>(finish - start);
+
+        std::cout << "FunctionGenerator Efficiency: "
+                  << x.size() * n_loops / time_span.count() / 1e6
+                  << " million evaluations / sec\n";
+        std::ofstream f_out(func.name + "_expansion.dat");
+        for (size_t i = 0; i < 1000; ++i) {
+            double a = func.low + i * range / 1000;
+            f_out << a << "\t" << f(a) << std::endl;
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
-    using namespace std::chrono;
-    high_resolution_clock::time_point start = high_resolution_clock::now();
+    size_t n_el = 1000000;
 
-    double range[2] = {1e-10, 1000};
-    std::function<double(double)> inFunc = log;
-    FunctionGenerator<7, 4096> myLog(inFunc, range[0], range[1], 1E-12);
+    clk::time_point start = clk::now();
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(range[0], range[1]);
-    size_t n_el = 1000000;
-    int n_loops = 100;
+    std::uniform_real_distribution<> dis(0, 1);
     std::vector<double> x(n_el);
     for (size_t i = 0; i < n_el; ++i)
         x[i] = dis(gen);
-    high_resolution_clock::time_point finish = high_resolution_clock::now();
-    duration<double> time_span =
-        duration_cast<duration<double>>(finish - start);
+    clk::time_point finish = clk::now();
+    duration time_span = std::chrono::duration_cast<duration>(finish - start);
 
     std::cout << "RNG generation took " << time_span.count() << " seconds.\n";
 
-    std::cout << "First 10 test value deltas\n";
-    for (auto i = 0; i < 10; ++i) {
-        std::cout << "\t" << myLog(x[i]) - log(x[i]) << std::endl;
-    }
+    std::vector<func_t> funcs = {
+        {"log", 1e-15, 1000, log},
+        {"gsl_sf_bessel_J0", 0, 100, gsl_sf_bessel_J0},
+        {"gsl_sf_bessel_I0", 0, 100, gsl_sf_bessel_I0},
+        {"std::erfc", 0, 5, [](double x) { return std::erfc(x); }},
+        {"std::erf", 0, 5, [](double x) { return std::erf(x); }},
+        {"gsl_sf_airy_Ai", -20, 5,
+         [](double x) { return gsl_sf_airy_Ai(x, GSL_PREC_DOUBLE); }},
+        {"sin", 0, 2 * M_PI, sin}};
 
-    {
-        std::vector<double> res(x.size());
-        high_resolution_clock::time_point start = high_resolution_clock::now();
-
-        double sum = 0.0;
-        for (int irun = 0; irun < n_loops; ++irun) {
-            for (size_t i = 0; i < n_el; ++i) {
-                res[i] = log(x[i]);
-                sum += res[i];
-            }
-        }
-        std::cout << sum << std::endl;
-        high_resolution_clock::time_point finish = high_resolution_clock::now();
-        duration<double> time_span =
-            duration_cast<duration<double>>(finish - start);
-
-        std::cout << "Efficiency: " << n_el * n_loops / time_span.count() / 1e6
-                  << " million evaluations / sec\n";
-        std::cout << "System 'log' took " << time_span.count() << " seconds.\n";
-    }
-
-    {
-        std::vector<double> res(x.size());
-        high_resolution_clock::time_point start = high_resolution_clock::now();
-
-        double sum = 0.0;
-        for (int irun = 0; irun < n_loops; ++irun) {
-            for (size_t i = 0; i < n_el; ++i) {
-                res[i] = myLog(x[i]);
-                sum += res[i];
-            }
-        }
-        std::cout << sum << std::endl;
-        high_resolution_clock::time_point finish = high_resolution_clock::now();
-        duration<double> time_span =
-            duration_cast<duration<double>>(finish - start);
-
-        std::cout << "Chebyshev 'log' took " << time_span.count()
-                  << " seconds.\n";
-        std::cout << "Efficiency: " << n_el * n_loops / time_span.count() / 1e6
-                  << " million evaluations / sec\n";
+    for (auto func : funcs) {
+        std::cout << std::endl << func.name << std::endl;
+        timeit<8, 4096>(func, x);
     }
 
     return 0;
