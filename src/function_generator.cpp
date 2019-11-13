3288fd7959816fbbab21784389a8196f23f45d0b
Robert Blackwell <rblackwell@flatironinstitute.org>
2019-11-13 09:31:54 -0500

    Fix python compilation for C++ module

--- src/function_generator.cpp
+++ src/function_generator.cpp
@@ -7,13 +7,13 @@ namespace py = pybind11;
 #define TABLE_SIZE 4096
 
 PYBIND11_MODULE(FunctionGenerator, m) {
-    py::class_<FunctionGenerator<ORDER, TABLE_SIZE>>(m, "FunctionGenerator")
+    py::class_<FunctionGenerator<ORDER, TABLE_SIZE, double>>(m, "FunctionGenerator")
         .def(py::init<py::function, double, double, double, double, uint16_t>(),
              py::arg("fpy") = "", py::arg("a") = 0.0, py::arg("b") = 1.0,
              py::arg("tol") = 1e-10, py::arg("mw") = 1e-15,
              py::arg("error_model") = (uint16_t) FGError::ErrorModel::standard)
-        .def("__call__", &FunctionGenerator<ORDER, TABLE_SIZE>::operator())
-        .def("__call__", &FunctionGenerator<ORDER, TABLE_SIZE>::arr_call);
+        .def("__call__", &FunctionGenerator<ORDER, TABLE_SIZE, double>::operator())
+        .def("__call__", &FunctionGenerator<ORDER, TABLE_SIZE, double>::arr_call);
     ;
 }
 #undef ORDER
